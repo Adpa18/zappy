82bf2fbe375ba8a4a4c96554e313dde1df424ee9
Victor <victor.gouet@epitech.eu>
2016-06-15 11:16:45 +0200

    ok

--- src_server/Trantorien_Event/avance_event.c
+++ src_server/Trantorien_Event/avance_event.c
@@ -5,7 +5,7 @@
 ** Login   <gouet_v@epitech.net>
 ** 
 ** Started on  Wed Jun  8 07:49:13 2016 Victor Gouet
-** Last update Wed Jun 15 10:53:51 2016 Victor Gouet
+** Last update Wed Jun 15 11:16:42 2016 Victor Gouet
 */
 
 #include "../../include_server/trantorien_event.h"
@@ -18,9 +18,11 @@ int	avance_event(t_trantorien *trantorien, t_list *list,
   printf("trantorien avance: %d\n", trantorien->id);
   printf("prev:\nx:%d\ny:%d\n, orientation:%d\n",
 	 trantorien->pos.x, trantorien->pos.y, trantorien->orientation);
+
     move_by_dir(trantorien, command, getVectorDir(trantorien->orientation));
     send_message("ok\n", &(trantorien->ref->client->sock));
     ppo_event_to_all_monitor(trantorien, list);
+
   printf("trantorien avance: %d\n", trantorien->id);
   printf("prev:\nx:%d\ny:%d\n, orientation:%d\n",
 	 trantorien->pos.x, trantorien->pos.y, trantorien->orientation);
