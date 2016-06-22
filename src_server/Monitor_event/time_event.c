86e74d190dbc91731078695417be4a3272581b76
Victor <victor.gouet@epitech.eu>
2016-06-22 14:47:05 +0200

    changement %lu %ld dans le time

--- src_server/Monitor_event/time_event.c
+++ src_server/Monitor_event/time_event.c
@@ -5,7 +5,7 @@
 ** Login   <gouet_v@epitech.net>
 ** 
 ** Started on  Wed Jun 22 13:54:15 2016 Victor Gouet
-** Last update Wed Jun 22 14:06:00 2016 Victor Gouet
+** Last update Wed Jun 22 14:44:58 2016 Victor Gouet
 */
 
 #include "../../include_server/monitor_event.h"
@@ -16,7 +16,7 @@ int	time_event(t_monitor *monitor, t_list *list,
 {
   (void)list;
   (void)tab;
-  sendf_message(&(monitor->ref->client->sock), "time %lu\n",
-	        (unsigned long)command->time_launch);
+  sendf_message(&(monitor->ref->client->sock), "time %ld\n",
+	        (long)command->time_launch);
   return (0);
 }
