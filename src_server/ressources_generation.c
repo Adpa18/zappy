bb899acd5eb3a555d8517e36e7328c5cf35219b0
Victor <victor.gouet@epitech.eu>
2016-06-26 20:15:41 +0200

    on augmente les ressources

--- src_server/ressources_generation.c
+++ src_server/ressources_generation.c
@@ -5,7 +5,7 @@
 ** Login   <gouet_v@epitech.net>
 ** 
 ** Started on  Thu Jun 23 17:05:18 2016 Victor Gouet
-** Last update Sat Jun 25 19:25:39 2016 Victor Gouet
+** Last update Sun Jun 26 20:14:23 2016 Victor Gouet
 */
 
 #include "../include_server/server.h"
@@ -27,13 +27,13 @@ int		ressources_generation(t_list *list, t_map *map,
 	{
 	  if (rand() % proba == 0)
 	    {
-	      map->map[y][x].food += rand() % proba_food;
-	      map->map[y][x].linemate += rand() % 2;
-	      map->map[y][x].deraumere += rand() % 2;
-	      map->map[y][x].sibur += rand() % 2;
-	      map->map[y][x].mendiane += rand() % 2;
-	      map->map[y][x].phiras += rand() % 2;
-	      map->map[y][x].thystame += rand() % 2;
+	      map->map[y][x].food += (rand() % proba_food) + 7;
+	      map->map[y][x].linemate += (rand() % 4 == 0 ? 1 : 0);
+	      map->map[y][x].deraumere += (rand() % 4 == 0 ? 1 : 0);
+	      map->map[y][x].sibur += (rand() % 2 == 4 ? 1 : 0);
+	      map->map[y][x].mendiane += (rand() % 4 == 0 ? 1 : 0);
+	      map->map[y][x].phiras += (rand() % 2 == 4 ? 1 : 0);
+	      map->map[y][x].thystame += (rand() % 4 == 0 ? 1 : 0);
 	      bct_event_to_all_monitor(list, x, y);
 	    }
 	  ++x;
