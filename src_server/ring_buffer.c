1180e3a92a188bba1701a508c2e2ab7d494c45a9
Victor <victor.gouet@epitech.eu>
2016-06-26 19:53:48 +0200

    norme

--- src_server/ring_buffer.c
+++ src_server/ring_buffer.c
@@ -5,7 +5,7 @@
 ** Login   <gouet_v@epitech.net>
 ** 
 ** Started on  Fri Jun  3 09:49:21 2016 Victor Gouet
-** Last update Sun Jun 26 19:19:52 2016 Victor Gouet
+** Last update Sun Jun 26 19:53:17 2016 Victor Gouet
 */
 
 #include "../include_server/ring_buffer.h"
@@ -19,24 +19,8 @@ void	reset(t_ring_buffer *ring)
   bzero(ring->buffer, sizeof(ring->buffer));
 }
 
-/* static int		init_select_for_fd(fd_set *fds, int fd) */
-/* { */
-/*   struct timeval	timeout; */
-
-/*   FD_ZERO(fds); */
-/*   timeout.tv_sec = 0; */
-/*   timeout.tv_usec = 0; */
-/*   FD_SET(fd, fds); */
-/*   if (select(fd + 1, NULL, fds, NULL, &timeout) == -1) */
-/*     { */
-/*       return (-1); */
-/*     } */
-/*   return (0); */
-/* } */
-
 void		flush(t_ref *client)
 {
-  /* fd_set	fds; */
   struct pollfd	fds;
   int		rc;
 
@@ -45,14 +29,8 @@ void		flush(t_ref *client)
       reset(&(client->ring));
       return ;
     }
-  /* if (init_select_for_fd(&fds, client->client->sock.sock) == -1) */
-  /*   { */
-  /*     reset(&(client->ring)); */
-  /*     return ; */
-  /*   } */
   fds.fd = client->client->sock.sock;
   fds.events = POLLOUT;
-  
   if ((rc = poll(&fds, 1, 0)) == -1)
     {
       reset(&(client->ring));
@@ -63,11 +41,6 @@ void		flush(t_ref *client)
       send_message(client->ring.buffer, &(client->client->sock));
       reset(&(client->ring));
     }
-  /* if (FD_ISSET(client->client->sock.sock, &fds)) */
-  /*   { */
-      /* send_message(client->ring.buffer, &(client->client->sock)); */
-      /* reset(&(client->ring)); */
-    /* } */
 }
 
 void	bufferise(t_ref *client, char *data)
