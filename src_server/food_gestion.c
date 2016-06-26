279ddcfd507c3282fa1303ffde92ff3c74a5ce86
Victor <victor.gouet@epitech.eu>
2016-06-26 13:13:43 +0200

    test

--- src_server/food_gestion.c
+++ src_server/food_gestion.c
@@ -5,7 +5,7 @@
 ** Login   <gouet_v@epitech.net>
 ** 
 ** Started on  Mon Jun 13 10:45:15 2016 Victor Gouet
-** Last update Sat Jun 25 19:11:59 2016 Victor Gouet
+** Last update Sun Jun 26 11:18:35 2016 Victor Gouet
 */
 
 #include <stdio.h>
@@ -25,11 +25,11 @@ static int	trantorien_eat(t_trantorien *trantorien,
   if (is_time_out_in(trantorien->time_left_food, time_left))
     {
       trantorien->time_left_food = getTimeSeconds();
-     --(trantorien->inventaire.food);
-     if (trantorien->inventaire.food == -1)
-     	{
-     	  return (-1);
-     	}
+     /* --(trantorien->inventaire.food); */
+     /* if (trantorien->inventaire.food == -1) */
+     /* 	{ */
+     /* 	  return (-1); */
+     /* 	} */
     }
   return (0);
 }
