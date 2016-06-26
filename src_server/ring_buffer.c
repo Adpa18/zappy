6eacd69d989d94072ef86d47148ffab83a958aa5
Victor <victor.gouet@epitech.eu>
2016-06-26 19:20:54 +0200

    on utilise poll au lieu de select

--- src_server/ring_buffer.c
+++ src_server/ring_buffer.c
@@ -5,11 +5,12 @@
 ** Login   <gouet_v@epitech.net>
 ** 
 ** Started on  Fri Jun  3 09:49:21 2016 Victor Gouet
-** Last update Sun Jun 26 18:00:59 2016 Victor Gouet
+** Last update Sun Jun 26 19:19:52 2016 Victor Gouet
 */
 
 #include "../include_server/ring_buffer.h"
 #include "../include_server/server.h"
+#include <poll.h>
 
 void	reset(t_ring_buffer *ring)
 {
@@ -18,40 +19,55 @@ void	reset(t_ring_buffer *ring)
   bzero(ring->buffer, sizeof(ring->buffer));
 }
 
-static int		init_select_for_fd(fd_set *fds, int fd)
-{
-  struct timeval	timeout;
+/* static int		init_select_for_fd(fd_set *fds, int fd) */
+/* { */
+/*   struct timeval	timeout; */
 
-  FD_ZERO(fds);
-  timeout.tv_sec = 0;
-  timeout.tv_usec = 0;
-  FD_SET(fd, fds);
-  if (select(fd + 1, NULL, fds, NULL, &timeout) == -1)
-    {
-      return (-1);
-    }
-  return (0);
-}
+/*   FD_ZERO(fds); */
+/*   timeout.tv_sec = 0; */
+/*   timeout.tv_usec = 0; */
+/*   FD_SET(fd, fds); */
+/*   if (select(fd + 1, NULL, fds, NULL, &timeout) == -1) */
+/*     { */
+/*       return (-1); */
+/*     } */
+/*   return (0); */
+/* } */
 
 void		flush(t_ref *client)
 {
-  fd_set	fds;
+  /* fd_set	fds; */
+  struct pollfd	fds;
+  int		rc;
 
   if (client->ring.buffer[0] == 0 || client->client->sock.sock < 0)
     {
       reset(&(client->ring));
       return ;
     }
-  if (init_select_for_fd(&fds, client->client->sock.sock) == -1)
+  /* if (init_select_for_fd(&fds, client->client->sock.sock) == -1) */
+  /*   { */
+  /*     reset(&(client->ring)); */
+  /*     return ; */
+  /*   } */
+  fds.fd = client->client->sock.sock;
+  fds.events = POLLOUT;
+  
+  if ((rc = poll(&fds, 1, 0)) == -1)
     {
       reset(&(client->ring));
       return ;
     }
-  if (FD_ISSET(client->client->sock.sock, &fds))
+  if (rc == 1)
     {
       send_message(client->ring.buffer, &(client->client->sock));
       reset(&(client->ring));
     }
+  /* if (FD_ISSET(client->client->sock.sock, &fds)) */
+  /*   { */
+      /* send_message(client->ring.buffer, &(client->client->sock)); */
+      /* reset(&(client->ring)); */
+    /* } */
 }
 
 void	bufferise(t_ref *client, char *data)
